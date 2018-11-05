1d97c4ebdc652cba85800e84a1de6fccc360577b
Darek <dariuszniemczycki@gmail.com>
2018-11-05 21:07:27 +0100

    d

---
+++ src/main/java/io/apka/ioaplication/IoAplicationApplication.java
@@ -0,0 +1,12 @@
+package io.apka.ioaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoAplicationApplication.class, args);
+	}
+}
---
+++ src/test/java/io/apka/ioaplication/IoAplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package io.apka.ioaplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IoAplicationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
